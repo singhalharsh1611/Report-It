eb8a8ca67f55a0952ff6646911ad3a95e36d4aed
kickout <goyal08dhruv@gmail.com>
2025-04-19 00:09:58 +0530

    delete comment function added
    Signed-off-by: kickout <goyal08dhruv@gmail.com>

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
  import { authMiddleware } from "../middleware/authMiddleware.js";
- import { addComment, getComments } from "../controllers/commentController.js";
+ import { addComment, deleteComments, getComments } from "../controllers/commentController.js";
+import { authorizeroles } from "../middleware/rolesMiddleware.js";
  
  const commentRouter = express.Router({mergeParams:true});
  
  commentRouter.post('/', authMiddleware, addComment);
  commentRouter.get('/', getComments);
+ commentRouter.delete('/:id_comment',authMiddleware,authorizeroles("admin"), deleteComments);
  
  export default commentRouter;
