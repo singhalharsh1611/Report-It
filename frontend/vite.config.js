283e2039838daabcaddb0c50a1adc6b709c4f0a6
Harsh Singhal <singhalharsh1611@gmail.com>
2025-07-05 17:45:06 +0530

    added vercel config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
 import path from "path"
- // import tailwindcss from ""
- import react from "@vitejs/plugin-react"
- import { defineConfig } from "vite"
- 
- // https://vite.dev/config/
+// import tailwindcss from ""
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
-     alias: {
-       "@": path.resolve(__dirname, "./src"),
-     },
-   },
- })
+  css: {
+    postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
